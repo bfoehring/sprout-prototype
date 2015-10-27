eb488e4a6fa5c84b121e9eba6c7ba268f47a9774
Bill Foehring <bill.foehring@gmail.com>
2015-10-26 21:27:09 -0500

    modal?

---
+++ sprout-prototype.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+  $('.film').hide();
+  //$('.modal-container').hide();
+  
+  $('.settings').click(function(){
+    $('.film').fadeIn(250);
+  	//$('.modal-container').fadeIn(250);
+  });
+  
+  //$('.close').click(function(){
+    //$('.film').hide();
+  	//$('.modal-container').hide();
+  });
+  
+});
